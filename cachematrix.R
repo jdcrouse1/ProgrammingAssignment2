ec76c1efff31a6a387db61b06325e2cf69260336
jdcrouse1 <jacob.crouse93@gmail.com>
2020-05-24 21:44:04 -0500

    Test to see if I successfully created my branch

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Testing to see if I used branch feature correctly
 
 makeCacheMatrix <- function(x = matrix()) {
 
